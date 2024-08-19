148942f9981a971a6aad44e735eb4a1752ea58bf
FrancoFMV <francomilesqui@gmail.com>
2024-08-19 17:18:48 -0300

    Entregable de Acumuladores

---
+++ acumuladores/Acumuladores.java
@@ -0,0 +1,20 @@
+package acumuladores;
+
+public class Acumuladores {
+
+    public boolean hayInterseccionPorFila(int[][] mat, int[][] matSinInterseccionPorFila) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hayInterseccionPorFila'");
+    }
+
+    public boolean algunaFilaSumaMasQueLaColumna(int[][] mat, int columnaSumaMenosQueAlgunaFila) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'algunaFilaSumaMasQueLaColumna'");
+    }
+
+    public boolean todosMultiplosEnAlgunaFila(int[][] mat, int numExisteFilaDeMultiplos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'todosMultiplosEnAlgunaFila'");
+    }
+
+}
